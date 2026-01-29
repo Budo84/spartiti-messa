ed5dd291b5f92b6b352056d473b73fe2928123eb
Budo84 <lamanteafabio@gmail.com>
2026-01-29 23:52:20 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,28 @@
-const CACHE_NAME = 'spartiti-messa-v1';
+const CACHE_NAME = 'spartiti-messa-ultimate-v1';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './manifest.json',
-  './icon-192.png'
+  'https://fonts.googleapis.com/icon?family=Material+Icons+Round',
+  'https://apis.google.com/js/api.js',
+  'https://accounts.google.com/gsi/client'
 ];
 
-// Installa il Service Worker e scarica i file
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-// Serve i file dalla cache quando sei offline
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+      }));
+  }));
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
 });
