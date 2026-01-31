d91f2d0ddfa79c0f6ff52ab5d057e6f213c70b78
Budo84 <lamanteafabio@gmail.com>
2026-01-31 13:25:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,37 @@
-const CACHE_NAME = 'spartiti-messa-v20-restore';
+const CACHE_NAME = 'spartiti-messa-v21';
+
+
 const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './manifest.json',
-  './logo192.png',
-  './logo512.png',
-  'https://fonts.googleapis.com/icon?family=Material+Icons+Round',
-  'https://apis.google.com/js/api.js',
-  'https://accounts.google.com/gsi/client',
-  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+'./',
+'./index.html',
+'./manifest.json',
+'./logo192.png',
+'./logo512.png',
+'https://fonts.googleapis.com/icon?family=Material+Icons+Round',
+'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
 ];
 
+
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+self.skipWaiting();
+e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
+
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-      }));
-  }));
-  self.clients.claim();
+e.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.map(k => k!==CACHE_NAME && caches.delete(k)))
+)
+);
+self.clients.claim();
 });
 
+
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+// ⚠️ NON cache Google API
+if(e.request.url.includes('googleapis.com') || e.request.url.includes('accounts.google.com')){
+return;
+}
+e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
