d6e942ddf6434857ed073696fb04a97ef442bf4c
Budo84 <lamanteafabio@gmail.com>
2026-02-02 11:27:37 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'spartiti-messa-v34-multi-fix';
+const CACHE_NAME = 'spartiti-messa-v35-pdf-render';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -6,7 +6,8 @@ const ASSETS_TO_CACHE = [
   './logo192.png',
   './logo512.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons+Round',
-  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.min.js' /* NUOVA LIBRERIA */
 ];
 
 self.addEventListener('install', (e) => {
